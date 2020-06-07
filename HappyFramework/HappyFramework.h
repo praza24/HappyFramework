594ae751258d580410c8cd56790daa42eb78bd39
praza24 <prasathsrithar@hotmail.co.uk>
2020-06-07 18:11:28 +0100

    Created the framework

---
+++ HappyFramework/HappyFramework.h
@@ -0,0 +1,19 @@
+//
+//  HappyFramework.h
+//  HappyFramework
+//
+//  Created by Prasath Srithar on 07/06/2020.
+//  Copyright © 2020 Prasath Srithar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HappyFramework.
+FOUNDATION_EXPORT double HappyFrameworkVersionNumber;
+
+//! Project version string for HappyFramework.
+FOUNDATION_EXPORT const unsigned char HappyFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HappyFramework/PublicHeader.h>
+
+
